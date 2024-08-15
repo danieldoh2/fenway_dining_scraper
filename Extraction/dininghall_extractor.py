cd7aba5be557007598907a56664b96fceb75dc5f
danieldoh2 <danieldoh.inbox@gmail.com>
2024-08-15 11:27:52 -0500

    dsd

--- Extraction/dininghall_extractor.py
+++ Extraction/dininghall_extractor.py
@@ -4,3 +4,4 @@ import requests
 requ = requests.get("https://menus.sodexomyway.com/BiteMenu/Menu?menuId=34476&locationId=31992001&whereami=https://bufenway.sodexomyway.com/dining-near-me/campus-center-dining-room")
 
 print(type(requ), (requ))
+print('fl')
