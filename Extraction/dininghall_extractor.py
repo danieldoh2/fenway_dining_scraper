289cbbf64da109a0bea7b5f9f84132c8bce465ae
danieldoh2 <danieldoh.inbox@gmail.com>
2024-08-16 12:21:17 -0500

    added requirements.txt

--- Extraction/dininghall_extractor.py
+++ Extraction/dininghall_extractor.py
@@ -3,5 +3,9 @@ import requests
 
 requ = requests.get("https://menus.sodexomyway.com/BiteMenu/Menu?menuId=34476&locationId=31992001&whereami=https://bufenway.sodexomyway.com/dining-near-me/campus-center-dining-room")
 
-print(type(requ), (requ))
-print('fl')
+soup = BeautifulSoup(requ.text, 'html')
+
+# items = soup.find_all('div', {'class': 'col-xs-9'})
+# print(type(items))
+foods = soup.find_all('a', {'href': '#'})
+print(foods)
